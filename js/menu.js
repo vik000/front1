73d26004a4c3137d484393d086f149660eef9e3b
Sergio Fernández <sergio.fernandezj@gmail.com>
2017-07-05 00:22:46 +0200

    added smooth scroll

---
+++ js/menu.js
@@ -0,0 +1,39 @@
+var navbarItems = document.getElementsByClassName('navbar-item');
+
+for (var i = 0; i < navbarItems.length; i++) {
+    navbarItems[i].addEventListener('click', function (event) {
+        var sectionToGo = this.getElementsByTagName('a')[0].href.split("#");
+        
+        if (sectionToGo.length === 2) {
+            event.preventDefault();
+            var goTo = sectionToGo[sectionToGo.length - 1];
+            getElementByIdAndScroll(goTo);
+        }
+    });
+}
+
+function getElementByIdAndScroll (id) {
+    var elem;
+    if (id === '') {
+        elem = document.getElementsByClassName('header')[0];
+    } else {
+        elem = document.getElementById(id);
+    }
+
+    scrollToElement(elem);
+}
+
+function scrollToElement (element) {
+    var jump = parseInt(element.getBoundingClientRect().top * 0.3);
+    console.log(jump);
+
+    document.body.scrollTop += jump;
+
+    if (!element.lastJump || element.lastJump > Math.abs(jump)) {
+        element.lastJump = Math.abs(jump);
+
+        setTimeout(function() {
+            scrollToElement(element);
+        }, 40);
+    } 
+}
