7ba4d8d4864ee62a2f8c218e096088d8a91d1069
Sergio Fernández <sergio.fernandezj@gmail.com>
2017-07-06 21:11:42 +0200

    finished scroll spy

--- js/menu.js
+++ js/menu.js
@@ -45,3 +45,55 @@ function deleteActiveClass() {
         navbarItems[i].classList.remove('active');
     }
 }
+
+var acumulativeOffset = function (element) {
+    var top = 0;
+
+    do {
+        top += element.offsetTop || 0;
+        element = element.offsetParent;
+    } while (element);
+
+    return top;
+}
+
+var offsetQuienSoy = acumulativeOffset(document.getElementById('quien-soy')) - 50;
+var offsetEquipo = acumulativeOffset(document.getElementById('equipo')) - 50;
+var offsetTransporte = acumulativeOffset(document.getElementById('transporte')) - 50;
+var navbar = document.getElementsByClassName('navbar')[0];
+
+window.addEventListener('scroll', changeMenuStyle);
+
+var previous;
+function changeMenuStyle(event) {
+    var pageOffset = window.pageYOffset;
+
+    if (pageOffset >= 0 && pageOffset < offsetQuienSoy) {
+        if (!previous || previous !== 1) {
+            previous = 1;
+        } else if (previous === 1){
+            return false;
+        }
+        
+        deleteActiveClass();
+        document.querySelector("a[href='#']").parentNode.classList.add("active");
+    } else if (pageOffset >= offsetQuienSoy && pageOffset < offsetEquipo) {
+        if (!previous || previous !== 2) {
+            previous = 2;
+        } else if (previous === 2){
+            return false;
+        }
+        
+        deleteActiveClass();
+        document.querySelector("a[href$='quien-soy']").parentNode.classList.add("active");
+    } else if (pageOffset >= offsetEquipo &&  offsetQuienSoy < offsetTransporte) {
+        if (!previous || previous !== 3) {
+            previous = 3;
+        } else if (previous === 3){
+            return false;
+        }
+
+        deleteActiveClass();
+        document.querySelector("a[href$='equipo']").parentNode.classList.add("active");
+    }   
+}
